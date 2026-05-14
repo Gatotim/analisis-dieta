dc0dfd2b70e05ed0a0313e229e5c33cfd39a3c98
Carlos <carlosgabrielvazquezvelez@gmail.com>
2026-05-13 20:54:09 -0600

    feat(server): agregar backend con Express y Gemini API

---
+++ server.js
@@ -0,0 +1,30 @@
+require('dotenv').config()
+
+const {GoogleGenerativeAI} = require('@google/generative-ai')
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY)
+const model = genAI.getGenerativeModel({model: "gemini-3-flash-preview"})
+const express = require('express')
+const app = express()
+const cors = require('cors')
+app.use(cors())
+app.use(express.json({limit: '10mb'}))
+
+app.post('/analizar', async (req, res) => {
+    console.log('peticion recibida')
+    const { imagen } = req.body
+
+    const result = await model.generateContent([
+        {
+            inlineData: {
+                mimeType: 'image/jpeg',
+                data: imagen
+            }
+        },
+        '¿Que ingredientes tiene este alimento? Lista solo los ingredientes.'
+    ])
+    const respuesta = result.response.text()
+    res.json({ ingredientes: respuesta })
+})
+app.listen(8000, () => {
+    console.log('Servidor corriendo en el puerto 3000')
+})
